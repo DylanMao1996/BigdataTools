695cb482337ec8da68a6f8ff371763b005ff89f0
dylanmao <dylanmao@wisers.com>
2021-07-27 13:46:12 +0800

    add testcase

---
+++ src/test/java/Test.java
@@ -0,0 +1,10 @@
+/**
+ * @Auther: Dylan
+ * @Date: 2021/7/27 13:45
+ * @Description:
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("");
+    }
+}
